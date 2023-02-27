bda8e4032a6d602fb4742c100389fa19761afe9d
Herenyon <rikitur17@gmail.com>
2023-02-27 09:42:34 +0100

    ottimizzazione codice dadi

--- Dadi/js/main.js
+++ Dadi/js/main.js
@@ -1,18 +1,18 @@
-const dado = [1, 2, 3, 4, 5, 6];
 
-const dadoUtente = Math.floor(Math.random() * dado.length + 1);
-const dadoBot = Math.floor(Math.random() * dado.length + 1);
+
+const dadoUtente = Math.floor(Math.random() * 6 + 1);
+const dadoBot = Math.floor(Math.random() * 6 + 1);
 
 console.log(dadoUtente);
 console.log(dadoBot);
 
-if (dado[dadoUtente] > dado[dadoBot]){
+if (dadoUtente > dadoBot){
 
     console.log("Ha vinto l'utente con il numero" + ' ' + dadoUtente);
 
 }
 
-else if (dado[dadoUtente] < dado[dadoBot]){
+else if (dadoUtente < dadoBot){
 
     console.log("Ha vinto il Bot con il numero" + ' ' + dadoBot);
 
